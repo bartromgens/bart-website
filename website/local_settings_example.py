2b8497abbac861fecac8998f563c10ecb9ee53b4
Bart Römgens <bart.romgens@gmail.com>
2015-09-22 20:56:54 +0200

    Add STATIC_ROOT, STATIC_URL, MEDIA_ROOT, MEDIA_URL to local settings

--- website/local_settings_example.py
+++ website/local_settings_example.py
@@ -28,6 +28,26 @@ DATABASES = {
     }
 }
 
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = '/home/bart/webapps/bart_websitestatic/'
+
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+STATIC_URL = 'http://www.bartromgens.org/bart_websitestatic/'
+
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/home/media/media.lawrence.com/media/"
+MEDIA_ROOT = STATIC_ROOT + "media/"
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+MEDIA_URL = STATIC_URL + "media/"
+
+
 ###################
 # DEPLOY SETTINGS #
 ###################
