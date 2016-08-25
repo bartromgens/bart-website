5e254358243bf55ea52757cb56c8544197c8ac44
Bart Römgens <bart.romgens@gmail.com>
2016-08-25 08:52:09 +0200

    fix local settings example after merge conflict

--- website/local_settings_example.py
+++ website/local_settings_example.py
@@ -28,7 +28,6 @@ DATABASES = {
     }
 }
 
-<<<<<<< b74cb3d2ff2c53cf25e4b2eccecca6b759a6530f
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
